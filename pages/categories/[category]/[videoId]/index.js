e1bcc45343df99df3bd2478c5159462854cf1901
Jaron <jjkenyon777@gmail.com>
2022-10-31 17:00:32 -0700

    added to favorites popup

--- pages/categories/[category]/[videoId]/index.js
+++ pages/categories/[category]/[videoId]/index.js
@@ -12,8 +12,37 @@ import VideoList from '../../../../components/list/VideoList';
 import MobileList from '../../../../components/list/mobileList';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { Router, useRouter } from 'next/router';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { styled } from "@mui/material";
+
+
+
+
+const MyModal = styled("div") ({
+  position: 'relative',
+  borderRadius: 7,
+  width: 400,
+  backgroundColor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  "@keyframes myEffect": {
+    from: {
+      opacity: 0,
+      transform: "translateY(-200%)"
+    },
+    to: {
+      opacity: 1,
+      transform: "translateY(0)"
+    }
+  },
+  animation: 'myEffect 1s ease'
+});
 
 function SingleView(props) {
   const vidId = props.video[0].id;
@@ -21,8 +50,9 @@ function SingleView(props) {
   const [mobileView, setMobileView] = useState(false)
   const { query, push, router } = useRouter();
   const {id} = query
-
+  
   useEffect(() => { 
+    //favorites logic
     const favorites = JSON.parse(localStorage.getItem('favorites'))
     if (favorites) {
     if (favorites.includes(vidId)) {
@@ -35,10 +65,12 @@ function SingleView(props) {
     if(isMobile) {
       setMobileView(true)
     }}, [id,push])
+
 const channelLink = ('https://www.youtube.com/channel/' + props.video[0].snippet.channelId)
 const videoLink = ('https://www.youtube.com/embed/' + vidId);
 // const router = useRouter();
 
+//favorites button logic
 function addFavoriteHandler() {
   if (localStorage.getItem('favorites') === null) {
     localStorage.setItem('favorites', '[]')
@@ -47,7 +79,8 @@ function addFavoriteHandler() {
   oldData.push(vidId);
 
   localStorage.setItem('favorites', JSON.stringify(oldData));
-  setFavorite(true)
+  setFavorite(true);
+  handleOpen();
 }
 function removeHandler() {
   const favorites = JSON.parse(localStorage.getItem('favorites'))
@@ -57,10 +90,57 @@ function removeHandler() {
   localStorage.setItem('favorites', JSON.stringify(newArray))
   setFavorite(false)
 }
+// favorites modal 
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true)
+     setTimeout(() => {
+      handleClose()
+     }, 1200)
+  };
+  const handleClose = () => setOpen(false);
+  const FavoritesPopup = <div>
+  <Modal
+    aria-labelledby="transition-modal-title"
+    aria-describedby="transition-modal-description"
+    open={open}
+    onClose={handleClose}
+    closeAfterTransition
+   hideBackdrop={mobileView ? false : true}
+  >
+    <Fade in={open}>
+      <Box sx={{
+    position: 'absolute',
+    top: {xs: '13%',sm: '15%'},
+    left: '50%',
+    transform: 'translate(-50%, -50%)',}}>
+      <MyModal sx={{width: {xs: 300, sm: 400}}}>
+        <Typography id="transition-modal-description" variant="h6" sx={{ mt: .5, textAlign: 'center' }}>
+          Added to Favorites
+        </Typography>
+      </MyModal>
+      </Box>
+    </Fade>
+  </Modal>
+</div>
+
+const goBack = () => {
+  const back = router.back;
+  if (back === undefined) {
+    router.replace('/')
+  }
+  else {
+  router.back()
+}
+}
 
 if (mobileView) {
   return (
+    
     <Fragment>
+       {FavoritesPopup}
+
       <Paper elevation={5} sx={{
       alignItems: "center",
       justifyContent: 'center',
@@ -110,8 +190,10 @@ if (mobileView) {
 
   return (
     <Fragment>
+      {FavoritesPopup}
+
       <ArrowBackIosIcon 
-      onClick={() => router.back()}
+      onClick={goBack}
       sx={{margin: 3, marginLeft: 10, fontSize: 50,'&:hover': {
         cursor: "pointer",
       },}}/>
@@ -126,23 +208,23 @@ if (mobileView) {
       alignItems: "center",
       justifyContent: 'center',
       textAlign: 'center',
-      width: '80vh',
+      width: {xs:'50vh', md:'80vh'},
       margin: 'auto'
       }}>
     <iframe src={videoLink} frameBorder='0' width='100%' height='400' allowFullScreen></iframe>
-    <Box sx={{display: 'flex'}}>
-      <Box sx={{width: '50%'}}>
-    <Typography sx={{textAlign: 'start', fontWeight: 'bold', marginLeft: '1.5vh'}}>{props.video[0].snippet.channelTitle}</Typography>
-    <Typography sx={{textAlign: 'start', textDecoration: 'underline', fontSize: '1.2vh', marginTop: '-.8vh', marginLeft: '1.5vh'}}>Visit Channel</Typography>
+    <Box style={{display: 'flex', flexDirection: 'row'}}>
+      <Box style={{width: '50%'}}>
+    <Typography style={{textAlign: 'start', fontWeight: 'bold', marginLeft: '1.5vh'}}>{props.video[0].snippet.channelTitle}</Typography>
+    <Typography style={{textAlign: 'start', textDecoration: 'underline', fontSize: '1.2vh', marginTop: '-.8vh', marginLeft: '1.5vh'}}>Visit Channel</Typography>
     </Box>
-       <Box sx={{width: '50%', display: 'flex', justifyContent: 'end'}}>
+       <Box style={{width: '50%', display: 'flex', justifyContent: 'end', cursor: 'pointer'}}>
     {!favorite ? <AddIcon onClick={addFavoriteHandler} sx={{fontSize: '4vh',alignItems: 'center', marginRight: '2vh' }}/>
     : <RemoveIcon onClick={removeHandler} sx={{fontSize: '4vh',alignItems: 'center', marginRight: '2vh' }}/>
     }
  </Box>
     </Box>
     
-    <Accordion sx={{borderTop: 0,width: '80vh', textAlign: 'center', margin: 'auto'}}>
+    <Accordion sx={{borderTop: 0,width: '100%', textAlign: 'center', margin: 'auto'}}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
